1bf2ebce7cc109a59cf5b48750483eb039a36212
nicolas mayol <mayolnicolas8@gmail.com>
2021-05-24 04:12:21 -0300

    fixed errors

--- Code/pages/index.js
+++ Code/pages/index.js
@@ -5,8 +5,8 @@ export default function Home (){
     return (
         <>
         <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
+        <title>Evaluacion Frontend</title>
+        <link rel="icon" href="https://media-exp1.licdn.com/dms/image/C4D0BAQHO-se7LzpMfg/company-logo_200_200/0/1519900679278?e=2159024400&v=beta&t=MWLmX-9OFh6YAZ0TQb_opjgZjsjGOAjpKWXWvA0qKxw" />
         </Head>
         <div style={{justifyContent:'center',alignContent:'center',textAlign:'center', height:'100vh', marginTop:'30vh'}}>
             <h1>HOLA MUNDO!</h1>
--- Code/src/atoms/Video/Video.jsx
+++ Code/src/atoms/Video/Video.jsx
@@ -2,9 +2,9 @@ export default function Video (){
     return(
         <div className='videoContainer'>
         <iframe width="560" height="315" src="https://www.youtube.com/embed/FJRMldSmy-M" 
-        title="YouTube video player" frameborder="0" 
+        title="YouTube video player" frameBorder="0" 
         allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" 
-        allowfullscreen></iframe>
+        allowFullScreen></iframe>
         </div>
     )
 }
